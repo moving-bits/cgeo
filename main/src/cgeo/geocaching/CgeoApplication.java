fc6aca91f782f844af6445a92068ebe823a48f9b
moving-bits <github@moving-bits.net>
2020-10-23 19:41:18 +0200

    add toolargetool for further analysis reg. #9188

--- main/src/cgeo/geocaching/CgeoApplication.java
+++ main/src/cgeo/geocaching/CgeoApplication.java
@@ -18,6 +18,8 @@ import android.view.ViewConfiguration;
 
 import androidx.annotation.NonNull;
 
+import com.gu.toolargetool.TooLargeTool;
+
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
@@ -56,6 +58,9 @@ public class CgeoApplication extends Application {
 
         // Restore cookies
         Cookies.restoreCookies();
+
+        // for debugging purposes only
+        TooLargeTool.startLogging(this);
     }
 
     /**
